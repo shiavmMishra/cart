33a41d87e1f4ad50975d5cefa0dae4d48f5bf9e7
Shivam Mishra <shivamtag40@gmail.com>
2020-06-12 18:31:49 +0530

    More styles to the cart1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
+import CartItem from './CartItem';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <h1>Cart</h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <CartItem />
+    </div>
+  );
 }
 
 export default App;
