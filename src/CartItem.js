5d6e51f84491fe6701da3f2f7bce56d069026e9e
Shivam Mishra <shivamtag40@gmail.com>
2020-06-12 19:44:25 +0530

    Modify state(setState)

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,43 +1,74 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-  constructor(){ //Creating the constructor
-    super();//calling the parent constructor which is Component
-    this.state={
-      price:1999,
-      title:'Mobile Phone',
-      qty:'1',
-      img:''
+  constructor () {
+    super();  //Calling parent class constructor which is Componenet.
+    this.state = {
+      price: 999,
+      title: 'Mobile Phone',
+      qty: 1,
+      img: ''
     }
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
   }
-  increaseQuantity = ()=>{
-    console.log('this.state',this.state);
+  increaseQuantity = () => {  //Arrow function binds itself.
+    // this.state.qty += 1;
+    // console.log('this', this.state);
+    // setState form 1
+    // this.setState({
+    //   qty: this.state.qty + 1
+    // });
+
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1
+      }
+    });
+  }
+  decreaseQuantity = () =>{
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty - 1
+      }
+    });
   }
   render () {
-    const {price,title,qty} = this.state; //Accessing the constructor.
+    const { price, title, qty } = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
           <img style={styles.image} />
         </div>
         <div className="right-block">
-        <div style={ { fontSize: 25 } }>{title}</div>
-        <div style={ { color: '#777' } }>Rs {price}</div>
-        <div style={ { color: '#777' } }>Qty: {qty}</div>
+          <div style={ { fontSize: 25 } }>{title}</div>
+          <div style={ { color: '#777' } }>Rs {price} </div>
+          <div style={ { color: '#777' } }>Qty: {qty} </div>
           <div className="cart-item-actions">
             {/* Buttons */}
-            <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/svg/992/992651.svg"
-            onClick = {this.increaseQuantity}
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/992/992651.svg"
+              onClick={this.increaseQuantity}
             />
-            <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/1828/1828906.svg"/>
-            <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1214/1214428.svg"/>
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
+              onClick={this.decreaseQuantity}/>
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+            />
           </div>
         </div>
       </div>
     );
   }
 }
-//Object to add styles
+
 const styles = {
   image: {
     height: 110,
